01abd76d0f5a25e2b9d4d98d42e18b1cfc596752
Christina Ford <fordc@cs.uchicago.edu>
2018-07-08 21:45:53 -0400

    updates

--- hello.py
+++ hello.py
@@ -81,4 +81,6 @@ def result():
       return render_template("result.html", password = password)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
